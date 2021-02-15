41925f960fe041276e541e15022f35a37bab93f0
NastiaZe <anastasiazemskova4212gmail.com>
2021-02-15 12:24:17 +0300

    задание по дз 3.3

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,69 +1,47 @@
 package ru.netology.domain;
 
 public class Radio {
-    private int currentRadiostation;
-    private int maxRadiostation;
-    private int minRadiostation;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentStation;
     private int currentVolume;
-    private int maxVolume;
-    private int minVolume;
+    private int minVolume = 0;
+    private int maxVolume = 10;
+    private boolean on;
 
-    public void setMaxRadiostation(int maxStation) {
-        this.maxRadiostation = maxStation;
+
+    public int getMaxStation() {
+        return maxStation;
     }
 
-    public void setMinRadiostation(int minStation) {
-        this.minRadiostation = minStation;
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
     }
 
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
+    public int getMinStation() {
+        return minStation;
     }
 
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
     }
 
     public int getCurrentStation() {
-        return currentRadiostation;
+        return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation > maxRadiostation) {
+        if (currentStation > maxStation) {
+            this.currentStation = maxStation;
             return;
         }
-        if (currentStation < minRadiostation) {
+        if (currentStation < minStation) {
+            this.currentStation = minStation;
             return;
         }
-        this.currentRadiostation = currentStation;
+        this.currentStation = currentStation;
+
     }
-
-    public void setNextStation() {
-        int nextStation = currentRadiostation + 1;
-        if (nextStation > maxRadiostation) {
-            this.currentRadiostation = minRadiostation;
-        } else
-            this.currentRadiostation = nextStation;
-    }
-
-    public void setPreviousStation() {
-        int previousStation = currentRadiostation - 1;
-        if (previousStation < minRadiostation) {
-            this.currentRadiostation = maxRadiostation;
-        } else
-            this.currentRadiostation = previousStation;
-    }
-
-    public int getMaxStation() {
-
-        return maxRadiostation;
-    }
-
-
-    public int getMinStation() {
-        return minRadiostation;
-    }
-
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -71,36 +49,67 @@ public class Radio {
 
     public void setCurrentVolume(int currentVolume) {
         if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
             return;
         }
         if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
             return;
         }
         this.currentVolume = currentVolume;
     }
 
-    public void increaseCurrentVolume() {
-        int nextVolume = currentVolume + 1;
-        if (nextVolume > maxVolume) {
-            return;
-        }
-        this.currentVolume = nextVolume;
+    public int getMinVolume() {
+        return minVolume;
     }
 
-    public void decreaseCurrentVolume() {
-        int previousVolume = currentVolume - 1;
-        if (previousVolume < minVolume) {
-            return;
-        } else
-            this.currentVolume = previousVolume;
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
     }
 
     public int getMaxVolume() {
         return maxVolume;
     }
 
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
 
-    public int getMinVolume() {
-        return minVolume;
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void increaseStation() {
+        if (currentStation == maxStation) {
+            this.currentStation = minStation;
+            return;
+        }
+        currentStation++;
+    }
+
+    public void decreaseStation() {
+        if (currentStation == minStation) {
+            this.currentStation = maxStation;
+            return;
+        }
+        currentStation--;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume--;
     }
 }
